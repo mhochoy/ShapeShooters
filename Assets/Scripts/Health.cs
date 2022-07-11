76f3f3dc1f28b2eca2caca87127fff85891c09ba
Hochoy M <51301524+mhochoy@users.noreply.github.com>
2022-07-11 17:59:11 -0400

    Gameplay Update: Flashing Hittable Items
    - Anything with health on it now has the option to flash when hit

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,12 +7,17 @@ public class Health : MonoBehaviour
     public int health;
     public bool isDrawing;
     public bool Indestructible;
+    public Color HitColor;
     [SerializeField] GameObject HitEffectObj;
     [SerializeField] GameObject DeathEffectObj;
     ParticleSystem HitEffect;
     ParticleSystem DeathEffect;
+    SpriteRenderer Renderer;
+    Color originalColor;
 
     void Start() {
+        TryGetComponent<SpriteRenderer>(out Renderer);
+        originalColor = Renderer.color;
         if (HitEffectObj) {
             HitEffectObj.TryGetComponent<ParticleSystem>(out HitEffect);
         }
@@ -30,6 +35,7 @@ public class Health : MonoBehaviour
             return;
         }
         if (health - val > 0) {
+            StartCoroutine(DamageEffectSequence(Renderer, HitColor, .1f, 0));
             health -= val;
         }
         else {
@@ -38,17 +44,8 @@ public class Health : MonoBehaviour
     }
 
     public void Damage(int val, Vector2 point) {
-        if (Indestructible) {
-            SpawnHitEffect(point);
-            return;
-        }
-        if (health - val > 0) {
-            SpawnHitEffect(point);
-            health -= val;
-        }
-        else {
-            Die();
-        }
+        Damage(val);
+        SpawnHitEffect(point);
     }
 
     public void SpawnHitEffect(Vector2 hit_point) {
@@ -74,5 +71,28 @@ public class Health : MonoBehaviour
             return;
         }
         gameObject.SetActive(false);
+    }
+
+    IEnumerator DamageEffectSequence(SpriteRenderer sr, Color dmgColor, float duration, float delay)
+    {
+        // save origin color
+        Color originColor = sr.color;
+
+        // tint the sprite with damage color
+        sr.color = dmgColor;
+
+        // you can delay the animation
+        yield return new WaitForSeconds(delay);
+
+        // lerp animation with given duration in seconds
+        for (float t = 0; t < 1.0f; t += Time.deltaTime/duration)
+        {
+            sr.color = Color.Lerp(dmgColor, originalColor , t);
+
+            yield return null;
+        }
+
+        // restore origin color
+        sr.color = originalColor;
     }
 }
