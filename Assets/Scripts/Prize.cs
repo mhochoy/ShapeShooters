d459a204e856443547803d6fed4759ffe0d8cb08
Hochoy M <51301524+mhochoy@users.noreply.github.com>
2022-07-11 16:30:15 -0400

    Fixed Prize trigger bug
    - Trigger no longer sits in game world after being collected.

--- Assets/Scripts/Prize.cs
+++ Assets/Scripts/Prize.cs
@@ -16,15 +16,18 @@ public class Prize : MonoBehaviour
     public int value;
     AudioSource Audio;
     SpriteRenderer Renderer;
+    BoxCollider2D Collider;
 
     void Start() {
         Audio = GetComponent<AudioSource>();
         Renderer = PrizeObject.GetComponent<SpriteRenderer>();
+        Collider = GetComponent<BoxCollider2D>();
     }
 
     public void CollectPrize() {
         collected = true;
         Renderer.enabled = false;
+        Collider.enabled = false;
         Audio.Play();
         PrizeObject.SetActive(false);
     }
